bc87b23689a3bbbfb9ca2788ec565870a4324225
Quentin Zervaas <quentin@crunchybagel.com>
2020-04-23 21:54:58 +0930

    Implementing callback

--- Android/TracePrivately/app/src/main/java/com/example/traceprivately/MainActivity.java
+++ Android/TracePrivately/app/src/main/java/com/example/traceprivately/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.traceprivately;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.play.core.tasks.OnCompleteListener;
+import com.google.android.play.core.tasks.OnFailureListener;
+import com.google.android.play.core.tasks.OnSuccessListener;
 import com.google.android.play.core.tasks.Task;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ContactTracing.ContactTracingCallback {
+
+    ContactTracing tracing = new ContactTracing();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +39,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.button_start_tracing:
 
-//                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//                ContactTracing tracing = new ContactTracing();
-//
-//                Task<ContactTracing.Status> status = tracing.startContactTracing(pendingIntent);
+                Intent intent = new Intent(this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-                break;
+                Task<ContactTracing.Status> task = this.tracing.startContactTracing(pendingIntent);
+
+                task.addOnSuccessListener(new OnSuccessListener<ContactTracing.Status>() {
+                    @Override
+                    public void onSuccess(ContactTracing.Status result) {
+
+                    }
+                });
+
+                task.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+
+                    }
+                });
+
+                task.addOnCompleteListener(new OnCompleteListener<ContactTracing.Status>() {
+                    @Override
+                    public void onComplete(Task<ContactTracing.Status> task) {
+
+                    }
+                });
+
 
             case R.id.button_stop_tracing:
+
+                this.tracing.stopContactTracing();
                 break;
-                
+
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onContact() {
+        // TODO: Use getContactInformation() to get the info so it can be displayed
+    }
+
+    @Override
+    public void requestUploadDailyTracingKeys(List<ContactTracing.DailyTracingKey> keys) {
+        // TODO: Upload keys to server as this is only called after a positive diagnosis
+    }
+
+    @Override
+    public void requestProvideDiagnosisKeys() {
+        // TODO: Add all of the infected keys from the server here using provideDiagnosisKeys()
     }
 }
